414d064019dce742da536997502cd7c21ed45ded
Sai Pc <schand31@asu.edu>
2015-09-06 22:06:48 -0700

    Added tests for duplicates

--- proj1/tests/binarySearchTest.js
+++ proj1/tests/binarySearchTest.js
@@ -9,6 +9,7 @@ describe('binary search', function () {
   beforeEach(function () {
     array = ["a", "b", "c", "d", "e", "f"];
     intArray = [1, 2, 3, 4, 5, 6, 7, 8];
+    floatArray = [1.0, 2.1, 3.2, 4.3, 5.6, 6.5, 7.2, 8.0];
   });
 
   it('returns the index of the element when element is present (string)', function () {
@@ -29,7 +30,6 @@ describe('binary search', function () {
   });
 
   it('returns the index of the element when element is present (float)', function () {
-    floatArray = [1.0, 2.1, 3.2, 4.3, 5.6, 6.5, 7.2, 8.0];
     pos = binarySearch(floatArray, 0, floatArray.length, 1.0);
     assert.equal(pos, 0);
 
@@ -46,6 +46,20 @@ describe('binary search', function () {
 
     pos = binarySearch(floatArray, 0, floatArray.length, 8.6);
     assert.equal(pos, -1);
+  });
+
+  it('returns the index of one of the elements when element is duplicated', function () {
+    array = ["a", "b", "c", "d", "f", "f"];
+    intArray = [1, 2, 2, 4, 5, 6, 8];
+    floatArray = [2.1, 2.1, 3.2, 4.3, 5.6, 6.5, 7.2];
+    pos = binarySearch(array, 0, array.length, "f");
+    assert.isTrue(pos === 4 || pos === 5);
+
+    pos = binarySearch(intArray, 0, intArray.length, 2);
+    assert.isTrue(pos === 1 || pos === 2);
+
+    pos = binarySearch(floatArray, 0, floatArray.length, 2.1);
+    assert.isTrue(pos === 0 || pos === 1);
   });
 
 });
