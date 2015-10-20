00abb869d2a2eaa5ebe8d362170be11c0d199e09
Sai Pc <schand31@asu.edu>
2015-10-19 17:27:59 -0700

    Added plato and istanbul yay

--- proj1/src/binarySearch.js
+++ proj1/src/binarySearch.js
@@ -4,18 +4,31 @@
 function binarySearch(array, start, end, element) {
   // takes in an array with indices that are integers
   // array can contain strings/floats/integers as values
+
+  // Data Flow Anomaly: multiple declarations of same variable
+  var mid = 1;
   var mid = Math.floor((start + end) / 2);
-  console.log("Searching from ",start, "to", mid , "to", end);
+  console.log('Searching from ', start, 'to', mid, 'to', end);
+  // comparison to self
+  if (mid < mid) {
+    //do nothing
+  }
+  // Data Flow Anomaly: unused and uninitialized variables
+  var a = 5 + 6;
+  var b;
+  // Data Flow Anomaly: unused expression
+  5 + 6;
+
   // strict equality, no type coercion
   if(array[mid] === element) {
-    console.log("Element found at ", mid);
+    console.log('Element found at ', mid);
     return mid;
   } else if (element < array[mid]) {
     return binarySearch(array, start, mid, element);
   } else if (element > array[mid]) {
     return binarySearch(array, mid + 1, end, element);
   } else {
-    console.log("No Element found");
+    console.log('No Element found');
     return -1;
   }
 }
